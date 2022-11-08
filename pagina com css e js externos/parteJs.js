eae6b379117274019d11fae4533310bf7b97fc71
Douglas Garcia <douglasgovmad@gmail.com>
2022-11-08 01:30:48 -0300

    pagina 2, botoes, funcoes no projeto

--- pagina com css e js externos/parteJs.js
+++ pagina com css e js externos/parteJs.js
@@ -1,3 +1,11 @@
 function myFunction() {
     document.getElementById("demonst").innerHTML = "Agora sou um sapo.";
 }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
