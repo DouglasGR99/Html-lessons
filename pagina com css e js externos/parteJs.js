c9b94576df4f4d206be680c17c4b842edc10ac0e
Douglas Garcia <douglasgovmad@gmail.com>
2022-11-08 01:45:27 -0300

    botoes verde, vermelho, mesmo projota

--- pagina com css e js externos/parteJs.js
+++ pagina com css e js externos/parteJs.js
@@ -2,6 +2,18 @@ function myFunction() {
     document.getElementById("demonst").innerHTML = "Agora sou um sapo.";
 }
 
+function myFunReverse() {
+    document.getElementById("demonst").innerHTML = "Voltei a ser um pato.";
+}
+
+function secretoVerde() {
+    document.getElementById("secreto").innerHTML = "E gosto de verde.";
+}
+
+function secretoVermelho() {
+    document.getElementById("secreto").innerHTML = "E gosto de vermelho.";
+}
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
